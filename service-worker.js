bd53493481661f0f700bc1c5aa0384c49b963b34
7vybnnz2ct-lgtm <7vybnnz2ct@privaterelay.appleid.com>
2026-07-19 20:37:00 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='baerflix-complete-v5-1';
+const CACHE='baerflix-baumhaus-v6';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
